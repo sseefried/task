a128ea058079b91379274b9c85ad54edcee1b3a3
Sean Seefried <sean.seefried@gmail.com>
2011-12-25 18:30:24 +1100

    Added module for parsing records from file.

--- src/Record.hs
+++ src/Record.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 --
 -- Author: Sean Seefried
 -- Date:   Wed 21 Dec 2011
@@ -8,4 +9,69 @@
 --
 module Record where
 
-data Record = Record { recId :: Text }
+-- standard libraries
+import Data.Aeson
+import Data.Aeson.Types (parseMaybe)
+import Data.Aeson.Encode (fromValue)
+import Blaze.ByteString.Builder (toByteString)
+import Data.Time
+import System.Locale (defaultTimeLocale)
+import Control.Monad (mzero)
+import Control.Applicative
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Attoparsec as AP
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Data.Maybe -- FIXME: Debug
+
+data Record = Record { recId     :: Text
+                     , recStart  :: UTCTime
+                     , recFinish :: UTCTime
+                     , recKeyValues :: [ (Text, Text) ] } deriving Show
+
+instance FromJSON Record where
+  parseJSON (Object v) =
+        Record <$> (v .: "id")
+               <*> (v .: "start"  >>= parseJSON)
+               <*> (v .: "finish" >>= parseJSON)
+               <*> (v .: "key_values" >>= parseJSON)
+  parseJSON _ = mzero
+
+instance ToJSON Record where
+  toJSON (Record id start finish keyValues) =
+    object ["id" .= id, "start" .= start, "finish" .= finish, "key_values" .= keyValues]
+
+parseRecord :: BS.ByteString -> Maybe Record
+parseRecord bs =
+  case AP.parse json bs of
+    AP.Done rest r -> parseMaybe parseJSON r :: Maybe Record
+    _              -> Nothing
+
+parser :: BS.ByteString -> [Record]
+parser = catMaybes . map parseRecord . BS.splitWith (=='\n')
+
+parseRecords :: BS.ByteString -> Map Text Record
+parseRecords = toMap . parser
+  where
+    toMap :: [Record] -> Map Text Record
+    toMap = foldl ins M.empty
+    ins m r = M.insert (recId r) r m
+
+parseRecordFile :: FilePath -> IO (Map Text Record)
+parseRecordFile path = BS.readFile path >>= (return . parseRecords)
+
+---------
+-- Tests
+
+testUTCTime :: UTCTime
+testUTCTime = fromJust $ parseTime defaultTimeLocale "%Y-%m-%d %H:%M" "2012-12-12 11:32"
+
+testRecord :: Record
+testRecord = Record "unique" testUTCTime testUTCTime []
+
+testRecordStr ::  BS.ByteString
+testRecordStr = toByteString . fromValue . toJSON $ testRecord
